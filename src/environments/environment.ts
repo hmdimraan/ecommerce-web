7f0bc274dc5977275f3a30c27483d30383f1636a
hmdimraan <231136@iiitt.ac.in>
2026-07-04 18:25:19 +0530

    updated changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   //apiUrl: ' https://localhost:7269/api',
   //imageUrl: 'https://localhost:7269/'
-  apiUrl: 'https://ecommerce-api-fm65.onrender.com/api',
+ apiUrl: 'https://ecommerce-api-fm65.onrender.com/api',
   imageUrl: 'https://ecommerce-api-fm65.onrender.com/'
 };
