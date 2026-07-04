79d4ab8df5d4e9229f8bbdae7335adc4c4782528
hmdimraan <231136@iiitt.ac.in>
2026-07-04 18:56:35 +0530

    fix image rendering issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   //apiUrl: ' https://localhost:7269/api',
   //imageUrl: 'https://localhost:7269/'
  apiUrl: 'https://ecommerce-api-fm65.onrender.com/api',
-  imageUrl: 'https://ecommerce-api-fm65.onrender.com/'
+  imageUrl: 'https://ecommerce-api-fm65.onrender.com/ProductImages/'
 };
